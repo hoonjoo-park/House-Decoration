872bbb7fd59688ec01009b3a4a4f39999db3451d
hoonjoo94 <hoonjoo94@naver.com>
2022-02-01 04:03:02 +0900

    🎉 project init

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const API = 'https://cdn.ggumim.co.kr/test/image_product_link.json';
---
+++ src/pages/introduceHome.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const IntroduceHome = () => {
+  return <div>집 소개하기</div>;
+};
